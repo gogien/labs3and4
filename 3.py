f55b4d6d29556f11feb2817b4e76ce9f418f51a0
gogingrisha@gmail.com <gogingrisha@gmail.com>
2023-12-12 19:19:51 +0300

    update

--- 3.py
+++ 3.py
@@ -1,40 +1,25 @@
 import csv
-
-# загрузка таблицы
-def load_table(file_path):
-    with open(file_path, 'r') as file:
-        reader = csv.reader(file)
-        table = [row for row in reader]
-    return table
-
-# сохранение таблицы
-def save_table(table, file_path):
-    with open(file_path, 'w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerows(table)
+import pickle
 
 
 class TableOperations:
+
     def __init__(self, table):
+        for row in table:
+            for i in range(len(row)):
+                row[i] = None if row[i] == '' else row[i]
         self.table = table
 
     def get_rows_by_number(self, start, stop=None, copy_table=False):
-        if not isinstance(start, int) or (stop is not None and not isinstance(stop, int)):
-            raise ValueError("Invalid input: start and stop should be integers.")
-        
-        if stop is not None and start > stop:
-            raise ValueError("Invalid input: start should be less than or equal to stop.")
-        
+        if stop is None:
+            stop = start
         if copy_table:
-            new_table = [row.copy() for row in self.table[start:stop]]
+            new_table = [row.copy() for row in self.table[start-1:stop]]
             return new_table
         else:
-            return self.table[start:stop]
+            return self.table[start-1:stop]
 
     def get_rows_by_index(self, *values, copy_table=False):
-        if not values:
-            raise ValueError("Invalid input: at least one value should be provided.")
-        
         if copy_table:
             new_table = [row.copy() for row in self.table if row[0] in values]
             return new_table
@@ -44,17 +29,15 @@ class TableOperations:
     def get_column_types(self, by_number=True):
         column_types = {}
 
-        if by_number:
-            for col_index in range(len(self.table[0])):
-                column_types[col_index] = self._get_column_type(col_index)
-        else:
-            for col_name in self.table[0]:
-                column_types[col_name] = self._get_column_type(col_name)
+        for col_index in range(len(self.table[0])):
+            if by_number:
+                column_types[col_index] = self.get_column_type(col_index)
+            else:
+                column_types[' '.join(row[col_index] for row in self.table)] = self.get_column_type(col_index)
 
         return column_types
 
-    def _get_column_type(self, column):
-        # Внутренняя функция для определения типа значений в столбце
+    def get_column_type(self, column):
         types = set()
         for row in self.table[1:]:
             value = row[column]
@@ -64,36 +47,39 @@ class TableOperations:
                 types.add(float)
             elif isinstance(value, bool):
                 types.add(bool)
+            elif value is None:
+                pass
             else:
                 types.add(str)
 
-        # Если встретились разные типы, вернем str
         return types.pop() if len(types) == 1 else str
 
     def set_column_types(self, types_dict, by_number=True):
-        if by_number:
-            for col_index, col_type in types_dict.items():
-                self._set_column_type(col_index, col_type)
-        else:
-            for col_name, col_type in types_dict.items():
-                self._set_column_type(col_name, col_type)
+        columns = {}
+        for index in range(len(self.table[0])):
+            columns[' '.join(row[index] for row in self.table)] = index
+        for col_index, col_type in types_dict.items():
+            if by_number:
+                self.set_column_type(col_index, col_type)
+            else:
+                self.set_column_type(columns[col_index], col_type)
 
-    def _set_column_type(self, column, new_type):
-        # Внутренняя функция для установки нового типа значений в столбце
+    def set_column_type(self, column, new_type):
         for row in self.table:
             value = row[column]
-            if new_type == int:
-                row[column] = int(value)
-            elif new_type == float:
-                row[column] = float(value)
-            elif new_type == bool:
-                row[column] = bool(value)
-            else:
-                row[column] = str(value)
+            if value is not None:
+                if new_type == int:
+                    row[column] = int(value)
+                elif new_type == float:
+                    row[column] = float(value)
+                elif new_type == bool:
+                    row[column] = bool(value)
+                else:
+                    row[column] = str(value)
 
     def get_values(self, column=0):
         result_column = []
-        column_type = self._get_column_type(column)
+        column_type = self.get_column_type(column)
 
         for row in self.table:
             value = row[column]
@@ -103,22 +89,189 @@ class TableOperations:
 
     def get_value(self, column=0):
         row = self.table[0]
-        column_type = self._get_column_type(column)
+        column_type = self.get_column_type(column)
         value = row[column]
         return column_type(value)
 
     def set_values(self, values, column=0):
-        column_type = self._get_column_type(column)
-        typed_values = [column_type(value) for value in values]
-
-        for i, row in enumerate(self.table):
-            row[column] = typed_values[i]
+        if column > max(len(row) for row in self.table)-1:
+            for i in range(column - max([len(row) for row in self.table])+1):
+                for row in self.table:
+                    row.append(None)
+            self.set_values(values, column)
+        else:
+            column_type = self.get_column_type(column)
+            typed_values = []
+            for value in values:
+                if value is not None and value != '':
+                    typed_values.append(column_type(value))
+                else:
+                    typed_values.append(None)
+            if len(typed_values) == len(self.table):
+                for i in range(len(self.table)):
+                    self.table[i][column] = typed_values[i]
+            elif len(typed_values) > len(self.table):
+                for i in range(len(self.table)):
+                    self.table[i][column] = typed_values[i]
+                typed_values = typed_values[len(self.table):]
+                for i in range(len(typed_values)):
+                    child = [None]*max(len(row) for row in self.table)
+                    child[0] = typed_values[i]
+                    self.table.append(child)
+            elif len(typed_values) < len(self.table):
+                for i in range(len(typed_values)):
+                    self.table[i][column] = typed_values[i]
+                for i in range(len(typed_values), len(self.table)):
+                    self.table[i][column] = None
 
     def set_value(self, value, column=0):
-        column_type = self._get_column_type(column)
+        column_type = self.get_column_type(column)
         typed_value = column_type(value)
-        self.table[column] = typed_value
+        self.table[0][column] = typed_value
+
+    def check_nones_rows(self):
+        flag = True
+        for item in self.table[-1]:
+            if item is not None:
+                flag = False
+                break
+        return flag
+
+    def add(self, *columns):
+        if all([self.get_column_type(column) in [int, float, bool] for column in columns]):
+            temp = columns[0]
+            columns = columns[1:]
+            print(columns)
+            for column in columns:
+                for j in range(len(self.table)):
+                    try:
+                        self.table[j][temp] += self.table[j][column]
+                    except TypeError:
+                        self.table[j][temp] = self.table[j][column] if self.table[j][temp] is None else self.table[j][temp]
+                    self.table[j][column] = '123'
+            for row in range(len(self.table)):
+                self.table[row] = list(filter(lambda x: x != '123', self.table[row]))
+        else:
+            raise TypeError('Нельзя выполнить сложение этих столбцов')
+
+    def sub(self, *columns):
+        if all([self.get_column_type(column) in [int, float, bool] for column in columns]):
+            temp = columns[0]
+            columns = columns[1:]
+            for column in columns:
+                for j in range(len(self.table)):
+                    try:
+                        self.table[j][temp] -= self.table[j][column]
+                    except TypeError:
+                        self.table[j][temp] = -self.table[j][column] if self.table[j][temp] is None else self.table[j][temp]
+                    self.table[j][column] = '123'
+            for row in range(len(self.table)):
+                self.table[row] = list(filter(lambda x: x != '123', self.table[row]))
+        else:
+            raise TypeError('Нельзя выполнить вычитание этих столбцов')
+
+    def mul(self, *columns):
+        if all([self.get_column_type(column) in [int, float, bool] for column in columns]):
+            temp = columns[0]
+            columns = columns[1:]
+            for column in columns:
+                for j in range(len(self.table)):
+                    try:
+                        self.table[j][temp] *= self.table[j][column]
+                    except TypeError:
+                        self.table[j][temp] = 0
+                    self.table[j][column] = '123'
+            for row in range(len(self.table)):
+                self.table[row] = list(filter(lambda x: x != '123', self.table[row]))
+        else:
+            raise TypeError('Нельзя выполнить умножение этих столбцов')
+
+    def div(self, *columns):
+        if all([self.get_column_type(column) in [int, float, bool] for column in columns]):
+            temp = columns[0]
+            columns = columns[1:]
+            for column in columns:
+                for j in range(len(self.table)):
+                    try:
+                        self.table[j][temp] /= self.table[j][column]
+                        self.table[j][column] = '123'
+                    except TypeError:
+                        if self.table[j][temp] is None:
+                            self.table[j][temp] = 0
+                            self.table[j][column] = '123'
+                        else:
+                            raise 'Делить на None нельзя'
+            for row in range(len(self.table)):
+                self.table[row] = list(filter(lambda x: x != '123', self.table[row]))
+        else:
+            raise TypeError('Нельзя выполнить деление этих столбцов')
 
     def print_table(self):
-        for row in self.table:
-            print(row)
+        for i in range(len(self.table)):
+            if i == len(self.table)-1 and self.check_nones_rows():
+                pass
+            else:
+                if all([row[-1] is None for row in self.table]):
+                    print('|'.join(list(map(str, self.table[i][:-1]))))
+                else:
+                    print('|'.join(list(map(str, self.table[i]))))
+
+
+class TableCsv(TableOperations):
+
+    def __init__(self, table):
+        super().__init__(table)
+
+    @staticmethod
+    def save_table(table, file_path):
+        with open(file_path, 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerows(table)
+
+    @staticmethod
+    def load_table(file_path):
+        with open(file_path, 'r') as file:
+            reader = csv.reader(file, delimiter=',')
+            table = [row for row in reader]
+        return table
+
+
+class TablePickle(TableOperations):
+
+    def __init__(self, table):
+        super().__init__(table)
+
+    @staticmethod
+    def load_table(file_path):
+        with open(file_path, 'rb') as file:
+            return pickle.load(file)
+
+    @staticmethod
+    def save_table(table, file_path):
+        with open(file_path, 'wb') as file:
+            pickle.dump(table, file)
+
+
+class TxtTable(TableOperations):
+
+    def __init__(self, table):
+        super().__init__(table)
+
+    @staticmethod
+    def save_table(table, file_path):
+        with open(file_path, 'w') as file:
+            for row in table:
+                file.write('|'.join(row)+'\n')
+
+
+data = TableCsv(TableCsv.load_table('test1.csv'))
+data.set_column_type(0, int)
+data.set_column_type(1, int)
+data.set_column_type(2, int)
+data.set_column_type(3, int)
+data.print_table()
+print()
+print(data.get_column_types())
+data.mul(0, 1, 2, 3)
+data.print_table()
+TableCsv.save_table(data.table, 'test2.csv')
